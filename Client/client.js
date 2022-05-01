8e45c35fa6237a41900bf6bfa944b8cb65a8ab3d
Tyriq <C00439145@louisiana.edu>
2022-05-01 01:11:32 -0500

    client can now see all messages

--- Client/client.js
+++ Client/client.js
@@ -13,7 +13,7 @@
 //document.querySelector() returns the first html object in the arguement.
 const btn = document.querySelector('button');
 const packet = document.querySelector('h2');
-
+const section = document.querySelector('section')
 //Since the string starts with an '#' it is now looking for the element by id name. 
 const measageBox = document.querySelector("#input");
 
@@ -24,12 +24,11 @@ const ws = new WebSocket('ws://localhost:8126/foo');
 //It also prints WebSocket Client Connected when socket is connected.
 ws.onopen = function() {
     console.log('WebSocket Client Connected');
-    ws.send('Hi this is web client.');
+    //ws.send('Hi this is web client.');
 };
 ws.onmessage = function(e) {
   console.log("Received: '" + e.data + "'");
-  packet.innerHTML = e.data;
-  
+  addElement(e.data)
 };
 
 //This function grabs the input from the messagebox and sends the input to the server.
@@ -37,6 +36,21 @@ sendMeasageToServer = () =>{
   const measage = measageBox.value;
   ws.send(measage);
 } 
+
+function addElement (message) {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode(message);
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  section.insertBefore(newDiv, currentDiv);
+}
 
 //When user press enter on keyboard while interacting with textbox the message is sent to server.
 input.addEventListener('keyup',function(e){
--- server.js
+++ server.js
@@ -15,9 +15,7 @@ wss.on('connection', function connection(ws) {
     wss.clients.forEach(function each(client) {
       //if (client.readyState === WebSocket.OPEN) {
         console.log(data.toString('utf8'));
-        console.log(client)
         client.send(data.toString('utf8'));
-        client.emit
       //}
     });
   });
